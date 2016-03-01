5f789ed9a8e05a62fdd13c49129b7827f8690532
Aluizio <aluizio777@gmail.com>
2016-03-01 10:06:21 -0300

    The class event is done.

--- event.py
+++ event.py
@@ -1,15 +1,40 @@
 #!/usr/bin/python
 
+from random import randint
+
 class Event():
 	graph=None
 	
-	def __init__(self):
-		pass
+	def __init__(self, size=4, max_weight=10):
+		self.graph = [[randint(1,max_weight) for x in range(size)] for y in range(size)]
+		for i in range(size):
+			for j in range(size):
+				if i == j:
+					self.graph[i][j] = 0
 		
 	
-	def f(self):
-		pass
+	def f(self, solution):
+		source = solution[0]
+		weight = 0
+		
+		for destiny in solution:
+			weight += self.graph[source][destiny]
+			source = destiny
+		
+		destiny = solution[0]
+		weight += self.graph[source][destiny]
+
+		return weight
 	
 	
 	def getInitialSolution(self):
-		pass
+		solution=[]
+		numbers_of_vertex = len(self.graph)
+ 
+		for i in range(numbers_of_vertex):
+			vertex = randint(0,numbers_of_vertex-1)
+			while vertex in solution:
+				vertex = randint(0,numbers_of_vertex-1)
+			solution.append(vertex)
+
+		return solution
